5da35e367af0b6e54799710be4cdbff1bde5399a
mveraalbarenque <don.marcos.vera@gmail.com>
2024-04-26 09:06:36 -0400

    Last UpLoad

--- src/App/Game/Display/Cards/index.tsx
+++ src/App/Game/Display/Cards/index.tsx
@@ -12,25 +12,39 @@ interface Props {
   data: Card[];
 }
 
-const Cards: React.FC<Props> = ({data}) => {
+const Cards: React.FC<Props> = ({ data }) => {
   const [columns, setColumns] = useState<number>(0);
 
-
   useEffect(() => {
-    const total = Math.ceil(Math.sqrt(data.length * 2))
+    const total = Math.ceil(Math.sqrt(data.length*2));
     setColumns(() => total);
   }, [data]);
 
   const renderCard = () => {
     return data.map((obj) => {
       const { id, name, img } = obj;
+      const propsImagenFront = {
+        className: styles.card,
+        key: id,
+        src: "/aws.svg",
+        alt: name,
+      };
       const propsImagen = {
         className: styles.card,
         key: id,
         src: img,
         alt: name,
       };
-      return <img {...propsImagen} />;
+      return (
+        <div>
+          {/* <div className={styles.front}>
+          <img {...propsImagenFront} />
+          </div> */}
+          <div className={styles.back}>
+            <img {...propsImagen} />
+          </div>
+        </div>
+      );
     });
   };
 
